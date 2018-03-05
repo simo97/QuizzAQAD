04b910d3c3ae581eb4fdbb33fb9141df8e80469e
adonis97 <simoadonis@gmail.com>
2018-03-06 00:18:48 +0100

    add static instead 2

--- quiz/settings.py
+++ quiz/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['quizzaquad.herokuapp.com']
 
 
 # Application definition
